31c564fcbeb76dd45f5a38fbfee8327bd147730f
mawag <wangbo2008@vip.qq.com>
2014-07-14 16:16:28 +0800

    build 07/14 --16:16

--- file/mycat.c
+++ file/mycat.c
@@ -1,13 +1,13 @@
 /*=============================================================================
 #   >>   文件名: mycat.c
-#   >> 	   描述: 
+#   >> 	   描述: cat的简单实现，-s -n -b 
 #   >>     作者: wangbo
 #   >>    Email: mawag@live.cn
 #   >>	   主页: http://hi.baidu.com/wangbo2008
 #   >>   Github: hithub.com/mawag
 #   >> 程序版本: 0.0.1
 #   >> 创建时间: 2014-07-13 17:30:44
-#   >> 修改时间: 2014-07-13 18:57:36
+#   >> 修改时间: 2014-07-14 16:04:37
 #  Copyright (c) wangbo  All rights reserved.
 =============================================================================*/
 
@@ -37,7 +37,7 @@ int main(int argc,char ** argv)
 	char *file;		//文件内容
 	int flag = 0;		//参数
 
-	//参数初始化
+	//参数检测
 	if(argc == 1)
 	{
 		printf("\"cat <file name>\"\n");
@@ -54,13 +54,18 @@ int main(int argc,char ** argv)
 					//参数部分
 					if(argv[i][j] == 'n')
 						flag += 1;
-					//else if(argv[i][j])
+					else if (argv[i][j] == 'b')
+						flag += 2;
+					else if (argv[i][j] == 's')
+						flag += 4;
 				}
 			}
 			else
 				path = argv[i];
 		}
 	}
+	if(flag == 7)
+		flag = 6;
 	if(argc == 2)
 	{
 		path = argv[1];
@@ -71,7 +76,7 @@ int main(int argc,char ** argv)
 		}
 	}
 	#ifdef DEBUG
-	printf("file name:%s\n",path);
+	printf("file name:%s, t=%d\n",path,flag);
 	#endif
 
 	//检查文件，读取
@@ -131,10 +136,22 @@ int main(int argc,char ** argv)
 	j = 0;
 	printf("filename:%s\ntype:%d\n",path,len);
 	printf(">>	start\n");
+	if((flag != 0)||(flag != 4))
+		printf("%5d\t",++j);
 	for(i=0;i<len;i++)
 	{
 		if(*(file - 1) == '\n')
-			printf("%3d:",j++);
+		{
+			if((flag == 1)||(flag == 5))
+				printf("%5d\t",++j);
+			else if(((flag == 2)||(flag == 3)||(flag == 6))&&(*file != '\n'))
+				printf("%5d\t",++j);
+		}
+		if((*(file - 1) == '\n')&&(*file == '\n')&&(*(file+1) == '\n')&&((flag == 4)||(flag == 5)||(flag == 6)))
+		{
+			file++;
+			continue;
+		}
 		printf("%c",*file++);
 	}
 	printf(">>	end\n");
